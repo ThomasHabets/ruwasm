7c1b23b301eadff9684a58703e0750f0b111687a
Thomas Habets <thomas@habets.se>
2026-03-26 20:13:23 +0000

    Add build-local.sh to build a local tgz

---
+++ build-local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+WEBD="web"
+PREFIX="ruwasm"
+TMPD="$(mktemp -d)"
+wasm-pack build --target web -d "$TMPD/$PREFIX" --dev
+cp "$WEBD/index.html" "$WEBD/worker.js" "$TMPD/$PREFIX/"
+(
+        cd "$TMPD" && tar czf - "$PREFIX"
+) > ruwasm.tgz
