d82b075549c0d19fe500adb82e59c3e7b04f74dc
Thomas Habets <thomas@habets.se>
2026-05-19 15:57:37 +0100

    Prettify HTML

--- build-local.sh
+++ build-local.sh
@@ -9,7 +9,7 @@ wasm-pack build --target web -d "$TMPD/$PREFIX" --dev
 #wasm-pack build --target web -d "$TMPD/$PREFIX" --release -- . -Z build-std=panic_abort,std
 #wasm-pack build --target web -d "$TMPD/$PREFIX" --release
 GIT="$(git describe --tags --dirty --always)"
-cp "$WEBD/index.html" "$WEBD/wasm-mod.js" "$TMPD/$PREFIX/"
+cp "$WEBD/index.html" "$WEBD/style.css" "$WEBD/wasm-mod.js" "$TMPD/$PREFIX/"
 sed -i "s/GIT_VERSION_NOT_SET/$GIT/g" "$TMPD/$PREFIX/index.html"
 (
         cd "$TMPD" && tar czf - "$PREFIX"
