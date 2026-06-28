580d760922242eed8a676ae9d4cc28e3e82b98aa
Thomas Habets <thomas@habets.se>
2026-06-28 21:34:56 +0100

    Use JS bootstrap code from rustradio-ui

--- build-local.sh
+++ build-local.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 WEBD="web"
 PREFIX="ruwasm"
+UI_MANIFEST="$(cargo metadata --format-version=1 | jq -r '.packages[] | select(.name=="rustradio-ui") | .manifest_path' | head -n1)"
+UI_DIR="$(dirname "$UI_MANIFEST")"
+UI_ASSETS="${UI_DIR}/assets"
+
 TMPD="$(mktemp -d)"
 if [[ ${1:-release} = "release" ]]; then
         wasm-pack build --target web -d "$TMPD/$PREFIX" --profiling
@@ -9,7 +14,13 @@ else
         wasm-pack build --target web -d "$TMPD/$PREFIX" --dev
 fi
 GIT="$(git describe --tags --dirty --always)"
-cp "$WEBD/index.html" "$WEBD/style.css" "$WEBD/wasm-mod.js" "$TMPD/$PREFIX/"
+cp \
+        "$WEBD/index.html" \
+        "$WEBD/style.css" \
+        "$WEBD/wasm-mod.js" \
+        "$TMPD/$PREFIX/"
+cp "$UI_ASSETS/bootstrap.js" "$TMPD/$PREFIX/rustradio-ui-bootstrap.js"
+
 sed -i "s/GIT_VERSION_NOT_SET/$GIT/g" "$TMPD/$PREFIX/index.html"
 (
         cd "$TMPD" && tar czf - "$PREFIX"
--- build-pages.sh
+++ build-pages.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 WEBD="web"
 PREFIX="ruwasm"
 OUTDIR="dist"
+UI_MANIFEST="$(cargo metadata --format-version=1 | jq -r '.packages[] | select(.name=="rustradio-ui") | .manifest_path' | head -n1)"
+UI_DIR="$(dirname "$UI_MANIFEST")"
+UI_ASSETS="${UI_DIR}/assets"
 
 rm -rf "$OUTDIR"
 mkdir -p "$OUTDIR"
@@ -12,6 +15,7 @@ mkdir -p "$OUTDIR"
 GIT="$(git describe --tags --dirty --always)"
 wasm-pack build --target web -d "$OUTDIR/$PREFIX"
 cp "$WEBD/index.html" "$WEBD/style.css" "$WEBD/wasm-mod.js" "$WEBD/coi-serviceworker.min.js" "$OUTDIR/$PREFIX/"
+cp "$UI_ASSETS/bootstrap.js" "$TMPD/$PREFIX/rustradio-ui-bootstrap.js"
 sed -i "s/GIT_VERSION_NOT_SET/$GIT/g" "$OUTDIR/$PREFIX/index.html"
 
 # publish contents of ruwasm/ at site root
